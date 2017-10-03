4c4d4488714c649b611f58da47d425ae2de127de
qq714772696 <714772696@qq.com>
2017-10-03 23:20:59 +0800

    完成后台系统

--- Beshop/src/cn/porkchop/filter/AdminFilter.java
+++ Beshop/src/cn/porkchop/filter/AdminFilter.java
@@ -1,6 +1,10 @@
 package cn.porkchop.filter;
 
+import cn.porkchop.domain.User;
+
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class AdminFilter implements Filter {
@@ -8,7 +12,7 @@ public class AdminFilter implements Filter {
     }
 
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
-/*        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) resp;
         User user = (User)request.getSession().getAttribute("user");
         if(user!=null&&"admin".equals(user.getUsername())){
@@ -16,10 +20,10 @@ public class AdminFilter implements Filter {
             return;
         }
         response.setContentType("text/html;charset=utf-8");
-        response.getWriter().write("没有权限!");*/
+        response.getWriter().write("没有权限!");
 
 
-        chain.doFilter(req, resp);
+        //chain.doFilter(req, resp);
 
     }
 
